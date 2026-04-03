f78f2e7313a5413dd2040a22f222e6aca113cb11
Apple 1 <apple1@Apples-MacBook-Air.local>
2026-04-03 17:58:12 +0530

    h

--- src/Component/Streem.jsx
+++ src/Component/Streem.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import './Style/Streem.css';
+import './Style/streem.css';
+// import "./Style/Exam.css";
 
 const Streem = () => {
   return (
